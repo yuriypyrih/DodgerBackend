adfba3e25f206f0d5e45746241bc8eb5101c83c0
Yuriy <yuriypyrih@gmail.com>
2021-10-10 14:29:29 +0300

    git init

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.send("Fuck you in particular");
+});
+
+app.listen(3000, ()=>{
+    console.log('Server started on port 3000')
+})
